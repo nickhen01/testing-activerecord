52c4e163f14aa825c43437050dc609c732f9ad7c
Nicolas Hennick <nicolashennick@gmail.com>
2019-01-02 09:30:21 +0000

    completed exercise

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,9 +1,18 @@
 require "spec_helper"
 
 describe Person do
-  it "requires a name to be valid" do
-    # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+  it "Not valid with nil name" do
+    person = Person.new(first_name: nil)
+    expect(person).to be_invalid
+  end
+
+  it "Not valid with a blank name" do
+    person = Person.new(first_name: "")
+    expect(person).to be_invalid
+  end
+
+  it "Valid with a first name" do
+    person = Person.new(first_name: "Paul")
+    expect(person).to be_valid
   end
 end
